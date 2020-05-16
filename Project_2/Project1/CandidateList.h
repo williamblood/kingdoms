887d4d0004c98a07628ed569842fc6a1d8d8f85a
GeezerChan <54392138+GeezerChan@users.noreply.github.com>
2020-05-15 23:47:03 -0700

    Update CandidateList.h

--- Project_2/Project1/CandidateList.h
+++ Project_2/Project1/CandidateList.h
@@ -25,6 +25,8 @@
 
 class CandidateList
 {
+	friend bool operator==(const CandidateType& obj, int id);
+
 public:
 
 	// Default constructor
@@ -49,17 +51,27 @@ public:
 	void printKingdomVotes(int id, int index) const;
 	// printCandidateTotalVotes
 	void printCandidateTotalVotes(int id) const;
-
 	// printFinalResults
 	void printFinalResults() const;
+
+	/*********************************************
+	* FUNCTION ADDED FOR SELECTION #6
+	*********************************************/
+	// printNumberOfVoters
+	void printNumberOfVoters() const;
+
 	// Destructor
 	~CandidateList();
 
-	
-
 private:
 	// searchCandidate
-	bool searchCandidate(int id, std::list<CandidateType>::const_iterator& itr) const;
+	bool searchCandidate(int id, 
+		std::list<CandidateType>::const_iterator& itr) const;
+	
+	// printHeader
+	void printHeader() const;
+	// printCandidate
+	void printCandidate(std::list<CandidateType>::const_iterator& itr, int pos) const;
 
 	std::list<CandidateType> candidates;
 };
